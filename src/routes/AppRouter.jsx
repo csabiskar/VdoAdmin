b8ca75c043011f029ae464fff0f74137fe2517f0
csabiskar <csabiskar@gmail.com>
2026-03-07 23:57:45 +0530

    Refactor AppRouter to streamline route definitions and remove redundant code

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/Layout/MainLayout";
 import AddProduct from "../pages/products/AddProduct";
 import ProductManager from "../ProductManager";
@@ -7,26 +7,6 @@ import Deals from "../pages/deals/Deals";
 import Blogs from "../pages/blogs/Blogs";
 import Orders from "../pages/orders/Orders";
 import CreateBlog from "../pages/blogs/createblogs";
-
-export const  AppRouter =createBrowserRouter([
-{
-    path:'/',
-    element:<MainLayout/>,
-    errorElement:<p>page not found...</p>,
-    children:[
-        {index:true,element:<AddProduct/>},
-        {path:"/categories",element:<Categories/>},
-        {path:"/deals",element:<Deals/>},
-        {path:"/blogs",element:<Blogs/>},    
-        {path:"/blogs/createblog",element:<CreateBlog/>},
-        { path: "/blogs/edit/:id", element: <CreateBlog /> },    
-        {path:"/product",element:<ProductManager/>},
-        {path:"/orders",element:<Orders/>},        
-
-    ]
-}
-
-])
 import Login from "../components/Login";
 import ProtectedRoute from "./ProtectedRoute";
 
@@ -44,6 +24,8 @@ export const AppRouter = createBrowserRouter([
       { path: "categories", element: <Categories /> },
       { path: "deals", element: <Deals /> },
       { path: "blogs", element: <Blogs /> },
+      { path: "blogs/createblog", element: <CreateBlog /> },
+      { path: "blogs/edit/:id", element: <CreateBlog /> },
       { path: "product", element: <ProductManager /> },
       { path: "orders", element: <Orders /> },
     ],
