19f622af4768ed30f68906de438e1504bf50be98
Raghu-KV <raghunandanv19@gmail.com>
2026-03-30 12:01:40 +0530

    live instabce

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://vdo-naturals.vercel.app/api",
+  baseURL: "https://vdo-naturals.vercel.app/api",
   // baseURL: "http://172.16.125.189:8080/api", // third floor WIFI
-  baseURL: "http://10.76.50.130:8080/api", // Vivo 1920
+  // baseURL: "http://10.76.50.130:8080/api", // Vivo 1920
   headers: { "Content-Type": "application/json" },
 });
 
